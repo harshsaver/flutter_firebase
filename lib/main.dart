c333a1cd947b0b6cb70067ab1ee8f57df4bc0a46
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2020-04-06 16:24:48 +0530

    See

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,44 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
-String text;
+String uText,dText;
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          TextField(),
-          RaisedButton(onPressed: (){}, child: Text("Button1"),),
-          Text(text),
-          RaisedButton(onPressed: (){}, child: Text("Button2"),),
+          TextField(onChanged: (text1){uText = text1;},),
+          RaisedButton(onPressed: (){_uploadToServer(uText);}, child: Text("Set"),),
+          Text(dText),
+          RaisedButton(onPressed: (){dText = getData();}, child: Text("Get"),),
         ],
       ),
     );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
   }
 }
 
 
 
-  void _uploadToServer(){
-
-
+  void _uploadToServer(String u){
+    FirebaseDatabase.instance.reference().child('testNode')
+        .set({
+      'title': 'Test Title',
+      'content': u
+    });
   }
+
+Future<String> getData() async {
+  String result = (await FirebaseDatabase.instance.reference().child("test").once()).value;
+  print("I got " + result);
+  return result;
+}
 
